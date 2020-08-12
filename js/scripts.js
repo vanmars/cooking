17ba2fefda2d70a5eddf6fad958730e9251c77f2
Vanessa Stewart <vstewart1213@gmail.com>
2020-08-12 14:52:27 -0700

    Change parseInt to parseFloat to allow for user input of decimals
    Co-authored-by: Jeff Dinsmore <hello@jeffdinsmore.com>
    Co-authored-by: Johnny Duverseau <duverseaujohnny21@gmail.com>

--- js/scripts.js
+++ js/scripts.js
@@ -16,23 +16,22 @@ function quartsToGallons(number){
 $(document).ready(function(){
   $("form#cups-form").submit(function(event){
     event.preventDefault();
-    const cups = parseInt($("input#cups").val());
+    const cups = parseFloat($("input#cups").val());
     const result = cupsToPints(cups);
     $("#pints-result").text(result);
   });
 
   $("form#pints-form").submit(function(event){
     event.preventDefault();
-    const pints = parseInt($("input#pints").val());
+    const pints = parseFloat($("input#pints").val());
     const result = pintsToQuarts(pints);
     $("#quarts-result").text(result);
   });
 
   $("form#quarts-form").submit(function(event){
     event.preventDefault();
-    const quarts = parseInt($("input#quarts").val());
+    const quarts = parseFloat($("input#quarts").val());
     const result = quartsToGallons(quarts);
     $("#gallons-result").text(result);
   });
- 
 });
